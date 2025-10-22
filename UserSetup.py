8d8a49f6a377343b33ac04cd844fe81fe818632e
lucaschrimes244-prog <Lucas.chrimes244@escapestudios.ac.uk>
2025-10-22 22:22:21 +0100

    Updated UserSetup, Fixed bugs in my code and added a layout cheat sheet

--- UserSetup.py
+++ UserSetup.py
@@ -1,4 +1,15 @@
+#For Vscode to maya
+print ("Loading userSetup Script")
+
 import maya.cmds as cmds
 
 cmds.commandPort(name="localhost:5678", stp="python")
 cmds.commandPort(name="localhost:7001", stp="mel")
+
+#Code to start my tool
+print ("Loading Smart Sheet Tool")
+
+def ToolStartup():
+    print ("Tool Was Started Successfully!")
+
+ToolStartup()
